908ba6b6910eb56f29793a4cdc876387b38a7f88
sgdg <seungdeng17@gmail.com>
2021-06-01 22:33:42 +0900

    chore: demo

--- src/codesandbox/Modal/Modal3.js
+++ src/codesandbox/Modal/Modal3.js
@@ -1,23 +1,24 @@
-import { useState } from "react";
-import Modal, { modalAnimation } from "../../lib";
-import "./modal.scss";
+import { useState } from 'react';
+import Modal, { modalAnimation } from '../../lib';
+import './modal.scss';
 
 export default ({ children }) => {
-  const [state, setState] = useState(false);
+    const [state, setState] = useState(false);
 
-  return (
-    <Modal
-      id={"Modal3"}
-      consumer={children}
-      state={state}
-      setState={setState}
-      duration={250}
-      animation={modalAnimation.slideDown}
-    >
-      <div className="modal-body">
-        😍
-        <button onClick={() => setState(false)}>Close</button>
-      </div>
-    </Modal>
-  );
+    return (
+        <Modal
+            id={'Modal3'}
+            consumer={children}
+            state={state}
+            setState={setState}
+            duration={250}
+            animation={modalAnimation.slideDown}
+            draggable={true}
+        >
+            <div className="modal-body">
+                😍
+                <button onClick={() => setState(false)}>Close</button>
+            </div>
+        </Modal>
+    );
 };
