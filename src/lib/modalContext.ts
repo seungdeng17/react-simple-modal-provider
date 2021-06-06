ff78ccc6cf3f8cd0bbd7e15cb7b5db1a7935dff3
sgdg <seungdeng17@gmail.com>
2021-06-06 17:55:27 +0900

    feat: typing

--- src/lib/modalContext.ts
+++ src/lib/modalContext.ts
@@ -1,17 +1,22 @@
 import { createContext, useContext } from 'react';
 import { ERROR_MESSAGES } from './constants';
 
+interface IModalContext {
+    open: ({}: {}) => void;
+    close: () => void;
+}
+
 const contextMap = new Map();
 
-const createModalContext = (id: string) => {
+const createModalContext = (id: string): React.Context<IModalContext> => {
     if (!id) throw new Error(ERROR_MESSAGES.MODAL_ID_INVALID_FROM_CONTEXT(id));
     if (contextMap.has(id)) return contextMap.get(id);
-    const context = createContext({});
+    const context = createContext<IModalContext>({} as IModalContext);
     contextMap.set(id, context);
     return context;
 };
 
-const useModal = (id: string) => {
+const useModal = (id: string): IModalContext => {
     if (!id) throw new Error(ERROR_MESSAGES.MODAL_ID_INVALID_FROM_USEMODAL(id));
     if (!contextMap.has(id)) throw new Error(ERROR_MESSAGES.MODAL_ID_NOT_EXIST(id));
     return useContext(contextMap.get(id));
